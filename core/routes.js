f8a634e9ce6dcdcedb9007b580fcb29f0412591f
βτεsηΘω Ðεsιɠη <bresnow@bresnow.local>
2021-12-02 20:40:27 -0500

    init

--- core/routes.js
+++ core/routes.js
@@ -1,26 +1,30 @@
+/* eslint-disable no-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
 var express = require('express')
+// eslint-disable-next-line no-unused-vars
 var debug = require('debug')('express-gun:routes')
 var Gun = require('gun')
 var router = express.Router()
 
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 
 var db = require('./db')
 var data = db.get('data')
 
 /* Router Settings */
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: true }))
 
-router.all('*',function(req,res,next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS");
-    next();
-});
+router.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS')
+  next()
+})
 
 router.get('/', (req, res) => {
+  	// eslint-disable-next-line no-tabs
   	res.sendStatus(200)
 })
 
@@ -28,23 +32,23 @@ router.get('/', (req, res) => {
 
 router.post('/api/set/:id', (req, res) => {
   try {
-	console.log(req.body)
-	data.path(req.params.id).put(req.body)
-	res.sendStatus(200)
-  } catch(e) {
-	console.log(e)
-	res.sendStatus(500)
+    console.log(req.body)
+    data.path(req.params.id).put(req.body)
+    res.sendStatus(200)
+  } catch (e) {
+    console.log(e)
+    res.sendStatus(500)
   }
 })
 
 router.get('/api/set/:id/:key/:value', (req, res) => {
   try {
-	var doc = {}; doc[req.params.key] = req.params.value;
-	data.path(req.params.id).put(doc)
-	res.sendStatus(200)
-  } catch(e) {
-	console.log(e)
-	res.sendStatus(500)
+    var doc = {}; doc[req.params.key] = req.params.value
+    data.path(req.params.id).put(doc)
+    res.sendStatus(200)
+  } catch (e) {
+    console.log(e)
+    res.sendStatus(500)
   }
 })
 
@@ -52,12 +56,12 @@ router.get('/api/set/:id/:key/:value', (req, res) => {
 
 router.get('/api/link/:id/:link', (req, res) => {
   try {
-	var link = data.path(req.params.link);
-	data.path(req.params.id).path('link').put(link)
-	res.sendStatus(200)
-  } catch(e) {
-	console.log(e)
-	res.sendStatus(500);
+    var link = data.path(req.params.link)
+    data.path(req.params.id).path('link').put(link)
+    res.sendStatus(200)
+  } catch (e) {
+    console.log(e)
+    res.sendStatus(500)
   }
 })
 
@@ -65,27 +69,27 @@ router.get('/api/link/:id/:link', (req, res) => {
 
 router.get('/api/get/:id/:key?', (req, res) => {
   try {
-	data.path(req.params.id).val(function(data){
-		if (!data) res.sendStatus(404)
+    data.path(req.params.id).val(function (data) {
+      if (!data) res.sendStatus(404)
 	  	else if (req.params.key) res.send(data[req.params.key])
 	  	else res.send(data)
-	}).not(function(){ res.sendStatus(404) });
-  } catch(e) {
-	console.log(e)
-	res.sendStatus(500);
+    }).not(function () { res.sendStatus(404) })
+  } catch (e) {
+    console.log(e)
+    res.sendStatus(500)
   }
 })
 
 router.get('/api/getlink/:id', (req, res) => {
   try {
-	data.path(req.params.id).path('link').map().val(function(data, key){
-		console.log("Link:", key, data);
-		if (!data) res.sendStatus(404)
+    data.path(req.params.id).path('link').map().val(function (data, key) {
+      console.log('Link:', key, data)
+      if (!data) res.sendStatus(404)
 	  	else res.send(data)
-	}).not(function(){ res.sendStatus(404) });
-  } catch(e) {
-	console.log(e)
-	res.sendStatus(500);
+    }).not(function () { res.sendStatus(404) })
+  } catch (e) {
+    console.log(e)
+    res.sendStatus(500)
   }
 })
 
@@ -93,14 +97,13 @@ router.get('/api/getlink/:id', (req, res) => {
 
 router.get('/api/unset/:id/:key?', (req, res) => {
   try {
-	data.path(req.params.id).put(null)
-	res.sendStatus(200)
-  } catch(e) {
-	console.log(e)
-	res.sendStatus(500)
+    data.path(req.params.id).put(null)
+    res.sendStatus(200)
+  } catch (e) {
+    console.log(e)
+    res.sendStatus(500)
   }
 })
-
 
 module.exports = app => {
   app.use(Gun.serve)
